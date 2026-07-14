bb05e1dcfe713aaf69a8f49e78f0dec3fa8978c6
HazzaGitHubisshit <harrymg1208@gmail.com>
2026-07-15 09:28:55 +1200

    FILE NAME REDIRECTORY FOR PC
    AND ANALOG PIN SKETCH

--- UNO/uno/uno.ino
+++ UNO/uno/uno.ino
@@ -1,30 +1,36 @@
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(13, OUTPUT);
+  //Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  /*int val = analogRead(A0);  // A0 with nothing connected — picks up noise
-  Serial.println(val);
-  delay(500);*/
-  digitalWrite(13, HIGH);
+  
+  digitalWrite(LED_BUILTIN, HIGH);
+  
+  delay(500);
+
+  digitalWrite(LED_BUILTIN, LOW);
+  
+  delay(500);
+  
+  /*digitalWrite(LED_BUILTIN, HIGH);
   Serial.println("light is on");
   delay(800);
-  digitalWrite(13, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
   Serial.println("light is off");
   delay(200);
-  digitalWrite(13, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
   Serial.println("light is on");
   delay(200);
-  digitalWrite(13, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
   Serial.println("light is off");
   delay(200);
-  digitalWrite(13, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
   Serial.println("light is on");
   delay(200);
-  digitalWrite(13, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
   Serial.println("light is off");
-  delay(800);
+  delay(800);*/
 }
